fcea752362e0728e33a3f0bed88733cf85b36b47
Oscar Keur <o.e.keur@gmail.com>
2021-08-10 18:25:40 +0200

    Fix nav buttons

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -10,19 +10,19 @@ export default function NavBar() {
     <Navbar fixed="top" className={styles.boxShadow}>
       <Heading size={1}>About me</Heading>
       <Navbar.Container align="right">
-        <Button as="a" href="https://github.com/oekeur">
+        <Button renderAs="a" href="https://github.com/oekeur">
           <Icon size={"large"}>
             <FontAwesomeIcon icon={faGithub} size={"lg"} />
           </Icon>
           <span>Github</span>
         </Button>
-        <Button as="a" href="https://www.linkedin.com/in/oscarkeur/">
+        <Button renderAs={"a"} href="https://www.linkedin.com/in/oscarkeur/">
           <Icon size={"large"}>
             <FontAwesomeIcon icon={faLinkedin} size={"lg"} />
           </Icon>
           <span>Linkedin</span>
         </Button>
-        <Button as="a" href="https://www.instagram.com/oekeu/">
+        <Button renderAs="a" href="https://www.instagram.com/oekeu/">
           <Icon size={"large"}>
             <FontAwesomeIcon icon={faInstagram} size={"lg"} />
           </Icon>
